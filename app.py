852050415b19ea9a4989ddf9a8060d84951721b9
Vijay-lanka <vijaylanka005@gmail.com>
2026-02-02 11:16:40 +0530

    Updated backend CORS config

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from analyze_resume import analyze_resume  # your new ML code
 app = FastAPI()
 
 origins = [
+    "https://pro-front-end-three.vercel.app/",  # your Vercel frontend URL
     "https://pro-front-final.vercel.app",  # your Vercel frontend URL
     "http://localhost:3000",               # for local testing
 ]
